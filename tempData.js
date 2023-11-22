dbd1ff3845c4f5d50d27aa00e3a461008e040971
kenechy <124642529+kenechy@users.noreply.github.com>
2023-11-22 14:38:57 +0800

    Innitial commit

---
+++ tempData.js
@@ -0,0 +1,19 @@
+export default tempData = [
+   
+    {
+        id: 1,
+      name: "Brieanne",
+      color: "#8022D9", 
+      todos: [
+        {
+          title: "Joyce Gwapa",
+          completed: true
+        },
+        {
+          title: "Alsonado Panget",
+          completed: false
+        }
+      ]
+    }
+  ];
+  
